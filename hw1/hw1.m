6b3b53113a7dbda242ababbe6b02b3bb4646494b
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-22 22:34:54 -0500

    Add solution for `Mother...?`

--- hw1/hw1.m
+++ hw1/hw1.m
@@ -13,3 +13,22 @@ a = abs(sin(pi / 3) + j / sec(-5 * pi / 3));
 l = 8 ^ (1 / 3);
 u = (2 * sum((1:80) / factorial(6))) ^ (1 / 2);
 m = (imag(floor(log(66 ^ (7j / 2))))) ^ 2;
+
+
+%% Mother...?
+A = [
+	a;
+	l;
+	u;
+	m;
+];
+F = [
+	A(1, 1), A(2, 1);
+	A(3, 1), A(4, 1);
+];
+T = transpose(F);
+B = inv(T * F);
+C = [
+	T, F;
+	F, T;
+];
