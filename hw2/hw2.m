324417ad083e2032bd9af3acb0c63b9fb4fe55f6
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-29 23:51:09 -0500

    Add solution for `Space`

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -6,3 +6,11 @@
 clc;
 close;
 close all;
+
+
+%% Space
+A         = reshape(exp(0:63), 8, 8);
+B         = A(3:6, 3:6);
+B_geomean = prod(B(:)) ^ (1 / numel(B));
+A(:, 1)   = flipud(A(:, 1));
+A(4:5, :) = [ ];
