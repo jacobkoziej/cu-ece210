3a534231b6ec53b658d2f5adb9709c84db2f8a26
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-03 18:08:02 -0500

    Add solution for `The Long Sssss`

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -64,3 +64,36 @@ surf(C(1:50, 1:50));
 title("Cool Title");
 xlabel("Funky x-axis");
 ylabel("Funky y-axis");
+
+
+%% The Long Sssss
+exp_100    = exp(-(linspace(0, 6.66, 100)    .^ 2));
+exp_10_000 = exp(-(linspace(0, 6.66, 10_000) .^ 2));
+
+erf_100    = erf(linspace(0, 6.66, 100));
+erf_10_000 = erf(linspace(0, 6.66, 10_000));
+
+diff_exp_100    = diff(exp_100)    ./ diff(linspace(0, 6.66, 100));
+diff_exp_10_000 = diff(exp_10_000) ./ diff(linspace(0, 6.66, 10_000));
+diff_erf_100    = diff(erf_100)    ./ diff(linspace(0, 6.66, 100));
+diff_erf_10_000 = diff(erf_10_000) ./ diff(linspace(0, 6.66, 10_000));
+
+diff_mse_100    = mean((diff_exp_100    - diff_erf_100)    .^ 2);  % 0.3832
+diff_mse_10_000 = mean((diff_exp_10_000 - diff_erf_10_000) .^ 2);  % 0.3833
+
+dx_100    = (6.66 - 0) / (100 - 1);
+dx_10_000 = (6.66 - 0) / (10_000 - 1);
+
+cumsum_exp_100      = (2 / sqrt(pi)) * (cumsum(exp_100)      * dx_100);
+cumsum_exp_10_000   = (2 / sqrt(pi)) * (cumsum(exp_10_000)   * dx_10_000);
+cumtrapz_exp_100    = (2 / sqrt(pi)) * (cumtrapz(exp_100)    * dx_100);
+cumtrapz_exp_10_000 = (2 / sqrt(pi)) * (cumtrapz(exp_10_000) * dx_10_000);
+
+cumsum_mse_100      = mean((cumsum_exp_100      - erf_100)    .^ 2);  % 1.9687e-03
+cumsum_mse_10_000   = mean((cumsum_exp_10_000   - erf_10_000) .^ 2);  % 1.9210e-07
+cumtrapz_mse_100    = mean((cumtrapz_exp_100    - erf_100)    .^ 2);  % 1.6877e-08
+cumtrapz_mse_10_000 = mean((cumtrapz_exp_10_000 - erf_10_000) .^ 2);  % 1.6373e-16
+
+plot(linspace(0, 6.66, 10_000), cumtrapz_exp_10_000);
+title("Cool Title");
+% cumtrapz() nets a better estimate as it uses a trapezoidal integration method
