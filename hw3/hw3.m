80a245003183d771ebb2bf70854f2e5a03e4e09b
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-20 18:21:27 -0500

    Add solution for `Space Between`

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -6,3 +6,17 @@
 clc;
 close;
 close all;
+
+
+%% Space Between
+A = reshape(exp(0:63), 8, 8);
+A(3:6, 3:6) = 0;
+geomean_A = geomean(A(A > 0));
+
+B = sin(linspace(0, 5, 100).*linspace(-5, 0, 100).');
+[_, I] = sort(abs(B - 1/2)(:));
+[X, Y] = ind2sub(size(B), I);
+
+f   = @(x, y) (1/4).*sqrt(x.^2 + y.^2);
+g   = @(x, y) exp(-(1 - x.*y).^2);
+vol = integral2(f, -4, 4, -4, 4) - integral2(g, -4, 4, -4, 4);
