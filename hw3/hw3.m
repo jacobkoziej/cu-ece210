913cb6a3fb77385feed75e8f0d490036bdcda925
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-20 19:12:06 -0500

    Add solution for `I Need a Vactation`

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -20,3 +20,33 @@ B = sin(linspace(0, 5, 100).*linspace(-5, 0, 100).');
 f   = @(x, y) (1/4).*sqrt(x.^2 + y.^2);
 g   = @(x, y) exp(-(1 - x.*y).^2);
 vol = integral2(f, -4, 4, -4, 4) - integral2(g, -4, 4, -4, 4);
+
+
+%% I Need a Vactation
+d = 256;
+a = 1:d;
+b = a';
+
+% moon
+A = true(d);
+A(sqrt((a - 99).^2 + (b - 99).^2) >= 29) = 0;
+
+% so true!!
+B = true(d);
+A(sqrt((a - 62).^2 + (b - 62).^2) >= 58) = 0;
+
+% vertical waves
+C = true(d);
+C(a - 4*sin(b/10) <= 200) = 0;
+
+% blue static
+S = rand(a(end), b(end), 3);
+S(:, :, 1:2) = 0;
+
+% more moon action
+M = A & B.';
+
+% setting the scene
+Z = (C .* S) + M;
+
+imshow(Z);
