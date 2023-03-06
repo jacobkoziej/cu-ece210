6ce56d0f88dd39c995eb3e4d70b4eabfba5e68de
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-05 19:09:15 -0500

    Add solution for `De-Gauss, Re-Gauss`

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -6,3 +6,30 @@
 clc;
 close;
 close all;
+
+
+%% De-Gauss, Re-Gauss
+x  = linspace(0, 2*pi, 1000);
+y1 = sin(x);
+y2 = gauss(x,      0, 1);
+y3 = gauss(x,   pi/2, 1);
+y4 = gauss(x,     pi, 1);
+y5 = gauss(x, 3*pi/4, 1);
+y6 = gauss(x,   2*pi, 1);
+
+V = [y2', y3', y4', y5', y6'];
+S = gram_schmidt(V);
+
+y7 = ortho_proj(y1, S);
+
+subplot(2, 1, 1);
+plot(x, y1);
+title('\(sin(x)\)', 'Interpreter', 'latex');
+xlabel('\(x\)', 'Interpreter', 'latex');
+ylabel('\(y\)', 'Interpreter', 'latex');
+
+subplot(2, 1, 2);
+plot(x, y7);
+title('\(sin(x)\) projection into \(S\)', 'Interpreter', 'latex');
+xlabel('\(x\)', 'Interpreter', 'latex');
+ylabel('\(y\)', 'Interpreter', 'latex');
