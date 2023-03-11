28ac2fdd257a66d2337b02e1d5de9ccaa6406d2b
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-11 02:49:10 -0500

    Add solution for `Pole-y Moley`

--- hw5/hw5.m
+++ hw5/hw5.m
@@ -29,3 +29,38 @@ stem(1:55, filter(b, a, samples));
 
 subplot(1, 2, 2);
 stem(1:55, conv(b / a, samples));
+
+
+%% Pole-y Moley
+load 'handel.mat'
+[y, fs] = audioread('handel.mat');
+soundsc(y, fs);
+
+% this appears to be a elliptic filter
+[b, a] = zp2tf(                                                         ...
+	[.74*exp(.76j); .74*exp(-.76j); .96*exp(1.24j); .96*exp(1.24)], ...
+	[exp(2.06j); exp(-2.06j); exp(1.43j); exp(-1.43j)],             ...
+	.1);
+[H, w] = freqz(b, a);
+
+Hdb = 20*log10(abs(H));
+Hph = rad2deg(unwrap(angle(H)));
+
+figure;
+subplot(2, 1, 1);
+plot(w, Hdb);
+xlabel("Frequency (rad)");
+ylabel("|H| (dB)");
+xlim([0 pi]);
+xticks([0 pi/2 pi]);
+xticklabels({'0', '\pi/2', '\pi'});
+title("Magnitude Response");
+
+subplot(2, 1, 2);
+plot(w, Hph);
+xlabel("Frequency (rad)");
+ylabel("Phase (deg)");
+xlim([0 pi]);
+xticks([0 pi/2 pi]);
+xticklabels({'0', '\pi/2', '\pi'});
+title("Phase Response");
