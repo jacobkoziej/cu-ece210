0eb6933953afdb50daad5feb6daf3f1ddaed172f
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-11 02:28:48 -0500

    Add solution for `Be Discrete`

--- hw5/hw5.m
+++ hw5/hw5.m
@@ -6,3 +6,26 @@
 clc;
 close;
 close all;
+
+
+%% Be Discrete
+b = [1/2, 2/3, 3/7];
+a = [2,   1/3, 1/2];
+
+% ROCs:
+% * {|z| < 1/2}
+% * {1/2 < |z| <= oo} (BIBO stable + causal)
+zplane(b, a);
+
+[z, p, k] = tf2zp(b, a);
+
+[H, T] = impz(b, a, 1:66);
+stem(T, H);
+
+samples = sin(floor((1:55) / 10));
+
+subplot(1, 2, 1);
+stem(1:55, filter(b, a, samples));
+
+subplot(1, 2, 2);
+stem(1:55, conv(b / a, samples));
