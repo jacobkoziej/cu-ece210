3a17e64270012c251602113d33272a6004c664f0
Jacob Koziej <jacobkoziej@gmail.com>
2023-04-02 14:32:18 -0400

    Add filter parameters

--- hw6/hw6.m
+++ hw6/hw6.m
@@ -6,3 +6,13 @@
 clc;
 clear;
 close all;
+
+
+% f_s = 44.1 kHz
+%
+% Class             | Type     | R_{pass} | R_{stop} | f_{pass}       | f_{stop}
+% ------------------+----------+----------+----------+----------------+---------------
+% Butterworth       | bandpass | 1 dB     | 50 dB    | f_s/6, f_s/3   | f_s/7, f_s/2.5
+% Elliptic          | bandstop | 1 dB     | 50 dB    | f_s/7, f_s/2.5 | f_s/6, f_s/3
+% Chebyshev Type I  | lowpass  | 5 dB     | 40 dB    | f_s/9          | f_s/8
+% Chebyshev Type II | highpass | 5 dB     | 40 dB    | f_s/3          | f_s/4
